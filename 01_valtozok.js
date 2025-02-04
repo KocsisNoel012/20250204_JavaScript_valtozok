912ff984d5abb87245450dcc7ee248518cf43bde
KNoel <kocsis.noel.kristof@diak.szbi-pg.hu>
2025-02-04 10:32:40 +0100

    valtozo peldak

---
+++ 01_valtozok.js
@@ -0,0 +1,19 @@
+var a = 2;
+console.log(a)
+
+
+let b = 3;
+b = 4;
+console.log(b);
+const c = 5;
+console.log(c)
+//c= 6; //hiba XD
+
+// Var változo ujradefiniálása
+var x =5;
+console.log(x);
+var x = "alma";
+console.log(x)
+
+let d = 3;
+console.log(d)
---
+++ 02_valtozo_tipusok.js
@@ -0,0 +1,36 @@
+let e = 2;
+console.log('e= '+ e + ' típusa ' + typeof e);
+
+const pi = 3.14
+console.log('pi= '+ pi + ' típusa ' + typeof pi);
+
+console.log(0.1+0.2);
+console.log((0.1+0.2).toFixed(2));
+
+console.log(1/0);
+console.log(-1/0);
+console.log(0/0);
+
+let firstname = 'John';
+let lastname = 'Doe';
+console.log(`Hello, ${firstname} ${firstname}`)
+let person = {
+    name: 'Charles Leclerc',
+    age: 27,
+    isStudent: false
+}
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+console.log(person.isStudent);
+console.log(person.address);
+
+let xy;
+console.log(xy)
+
+console.log('\nBOOLEAN');
+console.log(typeof true);
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(a));
+console.log(Boolean(1));
